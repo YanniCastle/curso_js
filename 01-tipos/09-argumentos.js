6df07435aeaeb09c67ae3ad72978e4d0f0a357d9
Yanni Castle <yannicastle5@gmail.com>
2024-07-09 22:28:57 -0600

    02 asignacion

--- 01-tipos/09-argumentos.js
+++ 01-tipos/09-argumentos.js
@@ -10,7 +10,9 @@ console.log(resultado);
 
 //Se puede manejar multiples parametros y multiples argumentos
 function sumas(a, b) {
+    console.log(arguments);//poco recomendable arguments
     return a + b;
 }
-let resultados = sumas(5, 6);
+let resultados = sumas(5, 6, 1, 2, 3);
 console.log(resultados);
+console.log(typeof suma);
---
+++ 02-opeadores/01-aritmeticos.js
@@ -0,0 +1,18 @@
+let a = 5;
+let b = 7;
+//agregando otro argumento a console 'adicion'
+// console.log(a + b, 'adicion');//adicion
+// console.log(a - b, 'resta');//resta
+// console.log(a * b, 'multiplicacion');//multiplicacion
+// console.log(a / b, 'division');//division
+// console.log(a % b, 'modulo');//modulo=residuo de division
+// console.log(a ** b, 'potencia');//potencia
+
+//incrementar
+console.log(++a);
+console.log(a++);
+console.log(a);
+//decrementar
+console.log(--a);
+console.log(a--);
+console.log(a);
